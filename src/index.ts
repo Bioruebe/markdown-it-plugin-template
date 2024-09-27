ce7b2b5186de3097d1b09a0c437290a18a6a77db
Bioruebe <Bioruebe@users.noreply.github.com>
2024-09-27 16:12:00 +0200

    Updated type imports to support markdown-it 14

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import type MarkdownIt from "markdown-it";
-import type Renderer from "markdown-it/lib/renderer";
-import type StateBlock from "markdown-it/lib/rules_block/state_block";
-import type Token from "markdown-it/lib/token";
+import type { Options, Renderer, StateBlock, Token } from "markdown-it";
 
 
 
-function renderPlugin(tokens: Token[], idx: number, options: any, env: any, self: Renderer) {
-	return self.renderToken(tokens, idx, options, env, self);
+function renderPlugin(tokens: Token[], idx: number, options: Options, env: any, self: Renderer) {
+	return self.renderToken(tokens, idx, options);
 }
 
 function plugin(state: StateBlock, startLine: number, endLine: number, silent: boolean) {
-
+	return false;
 }
 
 export default function myPlugin(md: MarkdownIt) {
